dfb8c0dc187dd076eb16a04dd2854be59b06fb25
Samson <ayenis@proton.me>
2025-03-08 13:09:33 -0600

    test: add test case for xml

--- app.py
+++
@@ -1,8 +0,0 @@
-from xml.etree.ElementTree import Element, iterparse
-from plugins.transformer.xml_transformer import XMLTransformer
-
-x = XMLTransformer()
-for event, elem in iterparse("test.log", events=["end"]):
-    r = x._transform(elem)
-    if r:
-        print(r)
