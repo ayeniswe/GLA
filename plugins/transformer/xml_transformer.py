7607604026a27a8741388e98a32739eb59d8ef6d
Samson <ayenis@proton.me>
2025-03-08 12:48:51 -0600

    feat: xml transformer support window events

--- plugins/transformer/xml_transformer.py
+++ plugins/transformer/xml_transformer.py
@@ -1,5 +1,5 @@
 from os import PathLike
-from typing import Optional
+from typing import Dict, Optional
 import dateparser
 from xml.etree.ElementTree import Element
 from typeguard import typechecked
@@ -11,9 +11,9 @@ from magic.magic import from_file
 
 
 @typechecked
-class JLUReader(Strategy):
+class JLU(Strategy):
     """
-    The `JLUReader` class is responsible for handling transformations
+    The `JLU` class is responsible for handling transformations
     of `Java Logging Util` xml DTD schema
     """
 
@@ -35,14 +35,72 @@ class JLUReader(Strategy):
 
 
 @typechecked
-class WinEventsReader(Strategy):
+class WinEvent(Strategy):
     """
-    The `WinEventsReader` class is responsible for handling transformations
+    The `WinEvent` class is responsible for handling transformations
     of `Windows Events` from `.evtx` logs
     """
 
+    NS = "{http://schemas.microsoft.com/win/2004/08/events/event}"
+
     def match(self, entry: Element) -> Optional[dict]:
-        pass  # TODO
+        if entry.tag != f"{self.NS}Event":
+            return None
+
+        res = {
+            "module": self._get_attribute(
+                entry, f"{self.NS}System/{self.NS}Provider", "Name"
+            ),
+            "level": self._to_lvl(
+                self._get_text(entry, f"{self.NS}System/{self.NS}Level")
+            ),
+            "source": self._get_text(entry, f"{self.NS}System/{self.NS}Computer"),
+            "timestamp": self._get_attribute(
+                entry, f"{self.NS}System/{self.NS}TimeCreated", "SystemTime"
+            ),
+        }
+
+        event_id = self._get_text(entry, f"{self.NS}System/{self.NS}EventID")
+        data_entries = entry.findall(f".//{self.NS}EventData/{self.NS}Data")
+
+        extra_data = {
+            data.get("Name"): data.text for data in data_entries if data.get("Name")
+        }
+        data_pieces = [
+            data.text for data in data_entries if not data.get("Name") and data.text
+        ]
+
+        message_parts = [f"EventID: {event_id}"]
+        if extra_data:
+            message_parts.append(str(extra_data))
+        if data_pieces:
+            message_parts.append(" ".join(data_pieces))
+
+        res["message"] = " - ".join(message_parts)
+        return res
+
+    def _to_lvl(self, level: str):
+        """Converts window events integer levels to respective log severity levels"""
+        if level == "1":
+            return "CRITICAL"
+        if level == "2":
+            return "ERROR"
+        if level == "3":
+            return "WARNING"
+        if level == "4":
+            return "INFO"
+        if level == "5":
+            return "DEBUG"
+        else:
+            return None
+
+    def _get_text(self, entry: Element, path: str) -> Optional[str]:
+        element = entry.find(f".//{path}")
+        return element.text if element is not None else None
+
+    def _get_attribute(self, entry: Element, path: str, attr: str) -> Optional[str]:
+        element = entry.find(f"./{path}")
+        return element.get(attr) if element is not None else None
 
 
 @typechecked
@@ -59,7 +117,7 @@ class XMLTransformer(BaseTransformer, Resolver):
         future log entries seen by this instance
         """
         super().__init__(
-            [JLUReader(), WinEventsReader()],
+            [JLU(), WinEvent()],
             cache,
         )
 
@@ -67,11 +125,11 @@ class XMLTransformer(BaseTransformer, Resolver):
         mapping: Optional[dict] = self.resolve(entry)
         if mapping:
             return Log(
-                level=mapping["level"],
-                module=mapping["module"],
-                source=mapping["source"],
-                timestamp=dateparser.parse(mapping["timestamp"]),
-                message=mapping["message"],
+                level=mapping.get("level"),
+                module=mapping.get("module"),
+                source=mapping.get("source"),
+                timestamp=dateparser.parse(mapping.get("timestamp")),
+                message=mapping.get("message"),
             )
 
     def _validate(self, path: PathLike) -> bool:
