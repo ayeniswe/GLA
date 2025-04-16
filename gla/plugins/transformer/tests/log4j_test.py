a22590e2bb2c2e5ed62b5b50cf5cdc59f4f2ba96
Samson Ayeni <samson.o.ayeni.ctr@army.mil>
2025-04-16 17:11:04 -0500

    wip: log4j tests

--- gla/plugins/transformer/tests/log4j_test.py
+++ gla/plugins/transformer/tests/log4j_test.py
@@ -7,67 +7,16 @@ def test_log4j_transformation(get_log_path, check_transformer_test_cases):
     log4j = Log4jTransformer()
     cases = [
         {
-            "expected": "2020-02-01T00:00:00 WARN [worker-thread] database.connection - Failed to connect to database",
+            "expected": "2020-03-01T00:00:00 WARN [worker-thread] database.connection - Failed to connect to database",
         },
         {
-            "expected": "2020-02-01T00:00:00 ERROR [main-thread] user.auth - Invalid login credentials",
+            "expected": "2020-03-01T00:00:00 ERROR [main-thread] user.auth - Invalid login credentials",
         },
         {
             "expected": "2020-03-01T00:00:00 DEBUG [api-thread] api.controller - Fetching user data",
         },
         {
-            "expected": "2020-01-02T00:00:00 INFO [network-thread] network.service - Server started successfully",
-        },
-        {
-            "expected": "2021-05-06T00:00:00 ERROR [io-thread] file.io - Error reading file",
-        },
-        {
-            "expected": "2021-07-08T00:00:00 WARN [worker-thread] user.profile - Missing profile information",
-        },
-        {
-            "expected": "2020-09-10T00:00:00 DEBUG [api-thread] api.controller - Creating new user",
-        },
-        {
-            "expected": "2020-02-01T00:00:00 ERROR server - Connection timeout",
-        },
-        {
-            "expected": "2021-03-11T00:00:00 INFO [main] application - System is healthy",
-        },
-        {
-            "expected": "2022-10-02T00:00:00 ERROR file.transfer - File not found",
-        },
-        {
-            "expected": "2020-03-01T00:00:00 WARN [logging-thread] logger.service - Log rotation failed",
-        },
-        {
-            "expected": "2020-03-01T00:00:00 ERROR [database-thread] db.query - SQL syntax error",
-        },
-        {
-            "expected": "2020-01-02T00:00:00 ERROR [main-thread] db.connect - Failed to connect to database",
-        },
-        {
-            "expected": "2021-09-10T00:00:00 ERROR [network-thread] api.request - Timeout error",
-        },
-        {
-            "expected": "2021-11-11T00:00:00 ERROR [network-thread] api.request - Timeout error occurred",
-        },
-        {
-            "expected": "2022-12-05T00:00:00 WARN [logging-thread] logger.service - Missing log file",
-        },
-        {
-            "expected": None,
-        },
-        {
-            "expected": None,
-        },
-        {
-            "expected": "2020-03-05T00:00:00 ERROR user.auth - Invalid session",
-        },
-        {
-            "expected": "2021-01-01T00:00:00 ERROR [api-thread] network.connect - Timeout",
-        },
-        {
-            "expected": "2022-08-15T00:00:00 ERROR api.auth - Unauthorized access",
+            "expected": "2020-03-01T00:00:00 INFO [network-thread] network.service - Server started successfully",
         },
     ]
     path = get_log_path("test-log4j.log")
@@ -76,18 +25,430 @@ def test_log4j_transformation(get_log_path, check_transformer_test_cases):
     log4j.resolve((path, "utf-8"))
 
     # ACT-ASSERT
-    # check_transformer_test_cases(cases, iterator, log4j)
-    for idx, entry in enumerate(iterator):
-        # ACT
-        result = log4j.transform(entry)
-        result = str(result) if result else result
+    check_transformer_test_cases(cases, iterator, log4j)
 
-        # ASSERT
-        try:
-            actual = cases[idx]["expected"]
-        except KeyError:
-            raise KeyError("Every expected case must have a single key named 'expected'")
-        except IndexError:
-            raise IndexError("An expected case must be specified for each iteration of tests")
-        assert (result == actual)
+def test_log4j_transformation2(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN [worker-thread] database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR [main-thread] user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG [api-thread] api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO [network-thread] network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j2.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation3(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN [worker-thread] database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR [main-thread] user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG [api-thread] api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO [network-thread] network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j3.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation4(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN [worker-thread] database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR [main-thread] user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG [api-thread] api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO [network-thread] network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j4.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation5(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j5.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation7(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j7.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
+
+def test_log4j_transformation6(get_log_path, check_transformer_test_cases):
+    # ARRANGE
+    log4j = Log4jTransformer()
+    cases = [
+        {
+            "expected": "2020-03-01T00:00:00 WARN database.connection - Failed to connect to database",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 ERROR user.auth - Invalid login credentials",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 DEBUG api.controller - Fetching user data",
+        },
+        {
+            "expected": "2020-03-01T00:00:00 INFO network.service - Server started successfully",
+        },
+    ]
+    path = get_log_path("test-log4j6.log")
+    iterator = Unstructured(path, "utf-8", "\n")
+    # Select correct strategy
+    log4j.resolve((path, "utf-8"))
+
+    # ACT-ASSERT
+    check_transformer_test_cases(cases, iterator, log4j)
 
